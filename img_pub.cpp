98d86b74e0f73286d53e38d00165fac1bd2e7269
izhengfan <mrzhengfan@gmail.com>
2018-12-03 14:35:11 +0800

    update image_pub

--- img_pub.cpp
+++ img_pub.cpp
@@ -5,23 +5,48 @@
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
-
+#include <boost/filesystem.hpp>
+using namespace std;
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "img_pub");
     ros::NodeHandle nh;
     image_transport::ImageTransport it(nh);
-    image_transport::Publisher pub = it.advertise("camera/image", 1);
-    cv::Mat image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);
-    cv::waitKey(30);
-    sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
-
-    ros::Rate loop_rate(5);
-    while (nh.ok()) {
-      pub.publish(msg);
-      ros::spinOnce();
-      loop_rate.sleep();
+    image_transport::Publisher pub = it.advertise("/camera/image_raw", 1);
+    vector<string> image_names;
+    int num_camera_images = 0;
+    std::string path(argv[1]);
+    string folder = path;
+    for (auto it = boost::filesystem::directory_iterator(folder);
+         it != boost::filesystem::directory_iterator(); it++)
+    {
+        if (!boost::filesystem::is_directory(it->path())) //we eliminate directories
+        {
+            std::string filename = it->path().filename().string();
+            if(boost::filesystem::extension(filename) == ".png")
+            {
+                num_camera_images++;
+                image_names.push_back(filename);
+            }
+        } else
+            continue;
     }
+    std::sort(image_names.begin(), image_names.end());
+    ros::Rate loop(20);
+    for(unsigned i=0; i<image_names.size(); i++)
+    {
+        string image_name = image_names[i];
+        cv::Mat image = cv::imread(folder + image_name, cv::IMREAD_GRAYSCALE);
+        // pub image
+        std_msgs::Header header;
+        header.stamp = ros::Time::now();
+        sensor_msgs::ImagePtr msg = cv_bridge::CvImage(header, "mono8", image).toImageMsg();
+        pub.publish(msg);
+        if(!ros::ok()) break;
+        ros::spinOnce();
+        loop.sleep();
 
+    }
+    return 0;
 }
 
